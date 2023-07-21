da41d3d9ea9142a2379fbb2f350592dc47d4b20c
BDisciplined <mlanzafame5@gmail.com>
2023-07-21 14:47:07 +0200

    [ADD] added event handler

--- index.js
+++ index.js
@@ -7,59 +7,27 @@ const {
 } = require("discord.js");
 
 const fs = require("node:fs");
+const path = require("node:path");
+
 const { token } = require("./config.json");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.commands = getCommands("./commands");
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith(".js"));
 
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, (c) => {
-  console.log(`Ready! Logged in as ${c.user.tag}`);
-});
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
 
-client.on(Events.InteractionCreate, (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-
-  let command = client.commands.get(interaction.commandName);
-
-  try {
-    if (interaction.replied) return;
-    command.execute(interaction);
-  } catch (error) {
-    console.error(error);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
   }
-});
+}
 
 // Log in to Discord with your client's token
 client.login(token);
-
-function getCommands(dir) {
-  let commands = new Collection();
-  const commandFiles = getFiles(dir);
-
-  for (const commandFile of commandFiles) {
-    const command = require(commandFile);
-    commands.set(command.data.toJSON().name, command);
-  }
-
-  return commands;
-}
-
-function getFiles(dir) {
-  const files = fs.readdirSync(dir, {
-    withFileTypes: true,
-  });
-
-  let commandFiles = [];
-
-  for (const file of files) {
-    if (file.isDirectory()) {
-      commandFiles = [...commandFiles, ...getFiles(`${dir}/${file.name}`)];
-    } else if (file.name.endsWith(".js")) {
-      commandFiles.push(`${dir}/${file.name}`);
-    }
-  }
-  return commandFiles;
-}
